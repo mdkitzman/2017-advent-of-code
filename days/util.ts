677bed1f2071d53734e5f5e7823a59979ae1d672
Mike Kitzman <mdkitzman@gmail.com>
2020-11-26 09:12:27 -0700

    day 13

--- days/util.ts
+++ days/util.ts
@@ -1,1 +1,4 @@
 export const sum = (prev: number, cur: number) => prev + cur;
+
+export const max = (prev: number, cur: number) => Math.max(prev, cur)
+export const min = (prev: number, cur: number) => Math.min(prev, cur)
